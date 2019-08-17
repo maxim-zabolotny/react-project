c2e84a165d7259402e13babdbef4155574728916
Maxim <maxim.zabolotny20121776@gmail.com>
2019-08-17 16:22:43 +0300

    change file app and messdes

--- Projects/01-first-project/react-kabzda-1/src/App.js
+++ Projects/01-first-project/react-kabzda-1/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
--- Projects/01-first-project/react-kabzda-1/src/components/Dialogs/Message/Message.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Dialogs/Message/Message.jsx
@@ -3,8 +3,8 @@ import s from "./../Dialogs.module.css";
 
 
 const Message = props => {
-  return <div className={s.dialog}>{props.message}</div>;
-};
+  return <div className={s.dialog}>{props.message}</div>
+}
 
 
 export default Message;
